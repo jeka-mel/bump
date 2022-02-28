e8b1d740faac9f990c5553d91a21cfdc15e3e970
Evgeniy <bilokon.evgeniy@gmail.com>
2022-02-28 10:01:33 +0200

    hash chages

--- hash.sh
+++ hash.sh
@@ -3,10 +3,16 @@
 
 commandos="docker-compose down ; sh doit.sh -m light  ; docker-compose up"
 
+current_state=`git pull`
+if [ "$current_state" = "Already up to date." ]; then
+    exit
+else
+    printf "${GREEN}Deployed!\n${RC}"
+fi
+
 if cmp --silent -- "temp.txt" "urls.txt"; then
   echo "files contents are identical"
 else
-  "curl https://api.telegram.org/bot162717732:AAFM1QiEiRMTYuVhal17vxg-mJHZ3FJBRWo/sendMessage?chat_id=@smsnotif&text=recreate containers"
-  # echo "recreate containers"
+  curl https://api.telegram.org/bot162717732:AAFM1QiEiRMTYuVhal17vxg-mJHZ3FJBRWo/sendMessage?chat_id=@smsnotif&text="$current_state"
   $commandos
 fi
