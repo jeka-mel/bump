d5450c9a689125e17e93f71c09d9b8b73aa984b9
Evgeniy <bilokon.evgeniy@gmail.com>
2022-02-28 10:41:25 +0200

    hash changes

--- hash.sh
+++ hash.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 # Ripper cron shell v1.0
 
-commandos="docker-compose down ; sh doit.sh -m light  ; docker-compose up"
-
+# shellcheck disable=SC2006
 current_state=`git pull`
 if [ "$current_state" = "Already up to date." ]; then
     exit
 else
-    printf "${GREEN}Deployed!\n${RC}"
-fi
-
-if cmp --silent -- "temp.txt" "urls.txt"; then
-  echo "files contents are identical"
-else
-  curl https://api.telegram.org/bot162717732:AAFM1QiEiRMTYuVhal17vxg-mJHZ3FJBRWo/sendMessage?chat_id=@smsnotif&text="$current_state"
-  $commandos
+    if cmp --silent -- "temp.txt" "urls.txt"; then
+      echo "files contents are identical"
+    else
+      echo "files contents are not identical"
+      docker_current_state=$(docker-compose down ; sh doit.sh -m light  ; docker-compose up)
+      echo "$docker_current_state"
+    fi
 fi
